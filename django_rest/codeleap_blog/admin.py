1189edc70dc82dad05a24353d5c954f7a604c1f8
vinivosh <vinicius7427@gmail.com>
2024-03-14 04:48:52 -0300

    Show Blogposts by decreasing date in admin page

--- django_rest/codeleap_blog/admin.py
+++ django_rest/codeleap_blog/admin.py
@@ -8,3 +8,5 @@ from codeleap_blog.models import *
 class BlogpostAdmin(admin.ModelAdmin):
     list_display = ('title', 'username', 'id', 'created_datetime')
     search_fields = ['id', 'username', 'title']
+
+    ordering = ['-created_datetime']
