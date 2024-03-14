f830eeef8efb043021e674f7702c5c9ad0cec2f3
vinivosh <vinicius7427@gmail.com>
2024-03-14 03:46:31 -0300

    Include URLs from codeleap_blog
    The URLS defined in codeleap_blog.urls.py are now included in the project's urls.py

--- django_rest/codeleap_blog/urls.py
+++ django_rest/codeleap_blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for codeleap project.
+URL configuration for codeleap_blog app.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Including URLs from other apps
-    path('', include('codeleap_blog.urls')),
+    # path('admin/', admin.site.urls),
 ]
