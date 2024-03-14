121748863c857d36b7cbefcf38c5362edaf1bdbe
vinivosh <vinicius7427@gmail.com>
2024-03-14 01:53:13 -0300

    Add ALLOWED_HOSTS to settings.py
    Limiting our ALLOWED_HOSTS

--- django_rest/codeleap/settings.py
+++ django_rest/codeleap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-y6qh6i5*yh=f-lmjai0p3k=7*li8+z8_eo8rtt9zk1x*v#ecd_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0.",
+    "localhost",
+]
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'codeleap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db-codeleap',
+        'PORT': 5432,
     }
 }
 
