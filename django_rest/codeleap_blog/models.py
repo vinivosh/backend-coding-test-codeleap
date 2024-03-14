b19dbf6eff0c6f4d5949bda71cf98fac00c40c08
vinivosh <vinicius7427@gmail.com>
2024-03-14 04:16:13 -0300

    Create models for Blogposts
    Following the specifications, the model for Blogposts has been...
    
    …created in codeleap_blog.models.py

--- django_rest/codeleap_blog/models.py
+++ django_rest/codeleap_blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.timezone import now as django_now
 
-# Create your models here.
+
+
+class Blogpost(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    username = models.CharField(max_length=50, verbose_name='Username')
+    created_datetime = models.DateTimeField(default=django_now, editable=False)
+    title = models.CharField(max_length=140)
+    content = models.TextField(max_length=2097152, verbose_name='') # Allowing roughly 2MB of ASCII text
